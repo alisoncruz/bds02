80d15b465375627a66de397e5c9513f6d13c5c98
alisoncruz <alisonn83@gmail.com>
2022-03-31 08:44:48 -0300

    Tests passing

---
+++ src/main/java/com/devsuperior/bds02/services/exceptions/DataBaseException.java
@@ -0,0 +1,14 @@
+package com.devsuperior.bds02.services.exceptions;
+
+public class DataBaseException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DataBaseException(String msg) {
+		super(msg);
+	}
+
+}
